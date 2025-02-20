20a900e8c6bdbc9875803b50a2f6fd309de17df2
J. Sieglaff <wolfen1842ia@gmail.com>
2025-02-20 15:33:05 -0600

    First Version of app

---
+++ JavaGui/src/FirstGUIApplication.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class FirstGUIApplication {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Java GUI Application");
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}
